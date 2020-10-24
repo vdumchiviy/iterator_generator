b090f03c014639524c479a9d005651ffc0d9cdd8
Maksim Kovtun <brain1999@mail.ru>
2020-10-24 15:34:20 +0300

    Changed main.py to use Decorator. Actualized README.MD

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from country import Countries
 from filestrmd5 import file_str_md5
+from debug import decorator_for_logging
 
 
+@decorator_for_logging('somelog.log')
 def country_wiki(file_name):
     file_countries = Countries(file_name)
     for item in file_countries:
         print(item)
 
 
+@decorator_for_logging('somelog2.log')
 def file_md5(file_name):
     for item in file_str_md5(file_name):
         print(item)
 
 
-country_wiki('countries.json')
+country_wiki(file_name='countries2.json')
 file_md5('countrywiki.json')
